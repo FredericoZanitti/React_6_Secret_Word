155c9e240eb09c9e2d718688003a8eb6cdb304af
FredericoZanitti <fredzanitti@gmail.com>
2022-12-26 16:19:37 +0000

    Secret Word

---
+++ src/App.js
@@ -0,0 +1,35 @@
+//CSS
+import './App.css';
+
+//React
+import { useState } from "react";
+
+//Data
+import { wordsList } from './data/words';
+
+//Components
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import StartScreen from './components/StartScreen';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+]
+
+function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
